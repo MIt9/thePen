6c0fc951a3e37cb442ad3a0bf8d638261066e57c
MIt9 <siniidrozd@gmail.com>
2015-04-25 05:40:09 +0300

    второй коммит
    второй коммит делает из этой ручки автоматическую ручку

--- script-pen.js
+++ script-pen.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Created by Dmitriy Bilukha on 25.04.2015.
  */
 
@@ -30,6 +30,38 @@ Pen.prototype.writeText = function (text) {
         console.log("Your writing tool is empty, recharge it")
     }
 };
+
+
+/***
+ * Автаматичиская ручка
+ * @param color - цвет задается как в css так "#000000" либо так "blue"
+ * @constructor
+ */
+
+function AutomaticPen(color) {
+    Pen.apply(this, arguments);
+    this.turnOn = false;
+    this.turn = function () {
+        return this.turnOn = !this.turnOn;
+    };
+}
+
+AutomaticPen.prototype = Object.create(Pen.prototype);
+AutomaticPen.prototype.constructor = AutomaticPen;
+
+
+AutomaticPen.prototype.writeText = function (text) {
+    if (this.turnOn) {
+        Pen.prototype.writeText.apply(this, arguments);
+    } else {
+        console.log("Your writing tool is turned off, and it can't write");
+    }
+    return this;
+};
+
+
+
+
 //======================Test===========================
 
 
@@ -44,3 +76,14 @@ var pen = new Pen("blue");
 testSupply(pen, 10);
 pen.rechargeInk();
 testSupply(pen, 2);
+
+
+console.log("\n==========AutomaticPen========");
+
+var ap = new AutomaticPen("green");
+console.log("Is Pen a prototype of AutomaticPen? "+Pen.prototype.isPrototypeOf(ap));
+testSupply(ap, 10);
+ap.turn();
+testSupply(ap, 10);
+ap.rechargeInk();
+testSupply(ap, 2);
